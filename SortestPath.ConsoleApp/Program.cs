2f4a79722acd45e94cf9f54c25add0dd6562d72d
Alex Petuschak <alex@swiftify.com>
2025-06-09 00:11:46 +0200

    - Cleanup

--- SortestPath.ConsoleApp/Program.cs
+++ SortestPath.ConsoleApp/Program.cs
@@ -4,11 +4,6 @@ using ShortestPath.BusinessLogic.Services;
 var service = new MovieService();
 service.LoadMovies();
 
-// foreach (var movie in service._movies)
-// {
-//     Console.WriteLine($"{movie.Title}: {string.Join(", ", movie.Cast.Select(a => a.Name))}");
-// }
-
 IEnumerable<IPathItem>? shortestPath = service.FindShortestPath("Javier Bardem", "Tom Cruise");
 
 Console.WriteLine(
