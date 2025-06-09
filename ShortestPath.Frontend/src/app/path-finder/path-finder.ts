1503d61118ece4daf57e46f8d44c344ae0cd0c99
Alex Petuschak <alex@swiftify.com>
2025-06-09 20:33:26 +0200

    * UI works

--- ShortestPath.Frontend/src/app/path-finder/path-finder.ts
+++ ShortestPath.Frontend/src/app/path-finder/path-finder.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Data } from '../../path-finder/data';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-path-finder',
+  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './path-finder.html',
   styleUrl: './path-finder.css'
@@ -14,11 +15,18 @@ export class PathFinderComponent {
   selectedActor: string = '';
   path: string[] = [];
 
-  constructor(private data: Data) {
-    this.actors = this.data.getActors();
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    // Get the list of actors from your API
+    this.http.get<string[]>('http://localhost:5255/api/path/actors')
+      .subscribe(data => this.actors = data);
   }
 
   findPath() {
-    this.path = this.data.getPathToTomCruise(this.selectedActor);
+    if (this.selectedActor) {
+      this.http.get<string[]>(`http://localhost:5255/api/path/path?fromActor=${encodeURIComponent(this.selectedActor)}`)
+        .subscribe(data => this.path = data);
+    }
   }
 }
